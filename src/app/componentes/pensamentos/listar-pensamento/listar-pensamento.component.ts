7593800ab49b150a6ebee2f3759f4c25a7b1b79b
DouglasIde <douglasymide@gmail.com>
2024-08-02 07:48:59 -0300

    Feat: Communitacting between components

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = []
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Estudar Alura todas as manhãs',
+      autoria: 'Alura',
+      modelo: 'modelo2'
+    }
+  ];
 
   constructor() { }
 
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  pensamento = {
+  @Input() pensamento = {
     conteudo: 'I Love Angular',
     autoria: 'Douglas',
     modelo: 'modelo3'
