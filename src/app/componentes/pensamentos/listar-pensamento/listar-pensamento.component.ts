38e4237c7dcb9740d49b1d589ba3a926593fb106
DouglasIde <douglasymide@gmail.com>
2024-08-07 15:25:32 -0300

    Fix: Fixing the error of creating thought

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Estudar Alura todas as manhãs',
-      autoria: 'Alura',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Testando',
-      autoria: 'Angular',
-      modelo: 'modelo1'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
