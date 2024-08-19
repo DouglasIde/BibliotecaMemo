173ce961b2368681e512f38f60d83580b8da3566
DouglasIde <douglasymide@gmail.com>
2024-08-19 15:11:12 -0300

    Feat: Paging and HttpParams

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,11 +10,12 @@ import { PensamentoService } from '../pensamento.service';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
@@ -11,8 +11,12 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos'
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<Pensamento[]>{
-    return this.http.get<Pensamento[]>(this.API);
+  listar(pagina: number): Observable<Pensamento[]>{
+    const itensPorPagina = 6;
+    let params = new HttpParams().set("_page", pagina).set("_limit", itensPorPagina)
+    // return this.http
+    //   .get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`);
+    return this.http.get<Pensamento[]>(this.API, {params})
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
