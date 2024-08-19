431af17fdefa172846abe39d1b886f081c29d191
DouglasIde <douglasymide@gmail.com>
2024-08-19 10:25:38 -0300

    Feat: Adding validators in Edit thought

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,7 +2,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PensamentoService } from '../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from '../../minusculoValidator/minusculoValidator';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -10,6 +11,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './editar-pensamento.component.scss'
 })
 export class EditarPensamentoComponent {
+
+  formulario!: FormGroup;
 
   pensamento: Pensamento = {
     id: 0,
@@ -21,15 +24,26 @@ export class EditarPensamentoComponent {
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private FormBuilder: FormBuilder
   ){ }
 
   ngOnInit(): void{
-    const id = this.route.snapshot.paramMap.get('id')
-    this.service.buscarPorId(id!).subscribe((pensamento) => {
-      this.pensamento = pensamento
+    this.formulario = this.FormBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        Validators.minLength(5)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        minusculoValidator
+      ])],
+      modelo: ['modelo1']
     })
-  }
+    }
+  
 
   editarPensamento(){
     this.service.editar(this.pensamento).subscribe(() => {
@@ -39,5 +53,13 @@ export class EditarPensamentoComponent {
 
   cancelar(){
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(){
+    if(this.formulario.valid){
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
   }
 }
