df25a063feb6b5be27eab9ce0d7a5a9bcb8d8bf7
DouglasIde <douglasymide@gmail.com>
2024-08-20 10:44:51 -0300

    Feat: Changing the title between my wall and my favorites

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -20,6 +20,7 @@ export class ListarPensamentoComponent implements OnInit {
   filtro: string = ''
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
 
   constructor(
     private service: PensamentoService,
@@ -50,6 +51,7 @@ export class ListarPensamentoComponent implements OnInit {
   }
 
   listarFavoritos(){
+    this.titulo = 'Meus Favoritos'
     this.haMaisPensamentos = true
     this.paginaAtual = 1
     this.favoritos = true;
