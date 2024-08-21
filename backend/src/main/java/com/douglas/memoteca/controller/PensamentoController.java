10352fa875bc82ef894bccdfeedadd2f41e50ee7
DouglasIde <douglasymide@gmail.com>
2024-08-21 15:41:41 -0300

    Feat: Creating the method to delete a thought

--- backend/src/main/java/com/douglas/memoteca/controller/PensamentoController.java
+++ backend/src/main/java/com/douglas/memoteca/controller/PensamentoController.java
@@ -1,18 +1,22 @@
 package com.douglas.memoteca.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.douglas.memoteca.model.Pensamento;
 import com.douglas.memoteca.repository.IPensamentoRepository;
@@ -58,6 +62,17 @@ public class PensamentoController {
 				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
 						.body(pensamentoRepository.save(pensamento)))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Pensamento> pensamento = pensamentoRepository.findById(id);
+		
+		if(pensamento.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		pensamentoRepository.deleteById(id);
 	}
 }
 
