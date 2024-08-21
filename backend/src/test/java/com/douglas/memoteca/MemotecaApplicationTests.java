294e092384762cd73422e90613898ed9568142fc
DouglasIde <douglasymide@gmail.com>
2024-08-20 21:35:39 -0300

    Feat: Adding the API using Spring Boot on the project

---
+++ backend/src/test/java/com/douglas/memoteca/MemotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.memoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
